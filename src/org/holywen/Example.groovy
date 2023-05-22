a17c2b7d6a4750fe0a67b0230139a95c8199304d
holywen <shaohua.wen@gmail.com>
2023-05-22 16:20:33 +0700

    call example1 from example class

--- src/org/holywen/Example.groovy
+++ src/org/holywen/Example.groovy
@@ -2,6 +2,8 @@ package org.holywen
 class Example {
    def helloWorld() {
       // Using a simple println statement to print output to the console
-      println('Hello World');
+      println('Hello World1');
+      def example1 = new Example1();
+      example1.helloWorld();
    }
 }
